694835e5f80a7b1f21097acd259d642db3ba71c9
Ningyi Chen <ningyichen@Ningyis-MacBook-Air.local>
2019-06-13 10:43:46 -0700

    create makeplot.py with vspace

--- GalaxyEffects/makeplot.py
+++ GalaxyEffects/makeplot.py
@@ -1,7 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import vplot
 import sys
+import string
+import subprocess as subp
+import matplotlib.pyplot as plt
+import vplot as vpl
+import numpy as np
 
 # Check correct number of arguments
 if (len(sys.argv) != 2):
@@ -12,25 +14,73 @@ if (sys.argv[1] != 'pdf' and sys.argv[1] != 'png'):
     print('ERROR: Unknown file format: '+sys.argv[1])
     print('Options are: pdf, png')
     exit(1)
-out = vplot.GetOutput()
-out2 = vplot.GetOutput('tides_only')
 
-plt.figure(figsize=(8.5,6))
-q = out.comp.SemiMajorAxis*(1-out.comp.Eccentricity)
-plt.semilogy(out.comp.Time/1e9,out.comp.SemiMajorAxis,'k--',zorder=100,label='Semi-major axis (with encounters)')
-plt.plot(out.comp.Time/1e9,q,'k-',zorder=100,label='Perihelion (with encounters)')
+nsemi=30
+necc=30
+semi=[0 for j in range(nsemi)]
+ecc=[0 for j in range(necc)]
+periq=[[0 for j in range(necc)] for k in range(nsemi)]
 
-q = out2.comp.SemiMajorAxis*(1-out2.comp.Eccentricity)
-plt.semilogy(out2.comp.Time/1e9,out2.comp.SemiMajorAxis,'--',c=vplot.colors.pale_blue,label='Semi-major axis (tide only)')
-plt.plot(out2.comp.Time/1e9,q,'-',c=vplot.colors.pale_blue,label='Perihelion (tide only)')
+result = subp.run("ls -d data/40Eri*", shell=True, stdout=subp.PIPE).stdout.decode('utf-8')
+dirs=result.split()
 
-plt.legend(loc='lower right', ncol=2, fontsize=10)
-plt.ylabel('Distance (au)')
-plt.xlabel('Time (Gyr)')
-plt.ylim(10,2e4)
+iSemi=0
+iEcc=0
+
+
+for dir in dirs:
+    if dir != "0":
+        cmd = "cd "+dir+"; vplanet vpl.in >& output"
+        subp.call(cmd, shell=True)
+         At this point the log file has been generated
+
+        print(dir)
+        sys.stdout.flush()
+        sys.stderr.flush()
+        # Now search for parameters
+        found=0
+
+        data = np.loadtxt('./data/grid_list.dat', skiprows=1, dtype=str, delimiter=' ',usecols=(1,2))
+        semi_data =data[::nsemi,0]
+        semi[iSemi]=float(semi_data[iSemi])
+        ecc_data =data[:necc,1]
+        ecc[iEcc] = float(ecc_data[iEcc])
+
+        data = np.loadtxt(dir+'/40Eri.comp.forward', dtype=float, delimiter=' ',usecols=(3))
+        periq[iEcc][iSemi]=float(min(data))
+        print("semi")
+        print(semi)
+        print("ecc")
+        print(ecc)
+        print("periq")
+        print(periq)
+
+        iSemi += 1
+        if (iSemi == nsemi):
+        # New line in ecc
+            iEcc += 1
+            iSemi = 0
+
+
+CS = plt.contourf(ecc,semi, periq,50)
+plt.ylabel('SemiMajorAxis',fontsize=20)
+plt.xlabel('Eccentricity',fontsize=20)
+plt.tick_params(axis='both', labelsize=20)
+plt.colorbar(CS)
+
+#plt.yscale('log')
+plt.xlim(0,0.9)
+plt.ylim(200,8000)
+
+
+x=[0,0.9]
+y=[418,418]
+plt.plot(x,y,linestyle='dotted',color='black',linewidth=3)
+
+plt.tight_layout()
+
 
 if (sys.argv[1] == 'pdf'):
     plt.savefig('GalaxyEffects.pdf')
 if (sys.argv[1] == 'png'):
     plt.savefig('GalaxyEffects.png')
-plt.close()
